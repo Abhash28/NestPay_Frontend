58486cf12b81fa6201d12bd69b3ac45c64d62062
Abhash Singh <abhashsingh28@gmail.com>
2026-01-20 17:04:20 +0530

    final admin commit

--- src/Pages/Admin/Property/PorpertyDetail/UnitAllocation/UnitAllocation.jsx
+++ src/Pages/Admin/Property/PorpertyDetail/UnitAllocation/UnitAllocation.jsx
@@ -84,7 +84,7 @@ const UnitAllocation = () => {
     try {
       const token = localStorage.getItem("token");
       await axios.post(
-        "http://localhost:5000/api/allocation/allocate",
+        "https://nestpay-backend.onrender.com/api/allocation/allocate",
         {
           propertyId,
           unitId: formData.unitId,
